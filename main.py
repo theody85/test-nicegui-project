07de7d990b7423a844fb6dbc5d6778fc79be3c2d
theody85 <gyambrahtheodora.8@gmail.com>
2025-09-05 12:25:50 +0000

    feat: project setup;

---
+++ main.py
@@ -0,0 +1,33 @@
+from nicegui import ui
+from sections import footer
+
+# using CSS
+with ui.row().style("flex-wrap:nowrap; width:100vw"):
+    ui.image("https://images.pexels.com/photos/30697854/pexels-photo-30697854.jpeg").style("width: 200px")
+    ui.label("Hello Python!")
+    ui.element("div").style('height:200px; width: 200px; background-color: red; border-radius:15px')
+
+    
+# using Tailwind
+with ui.column():
+    # ui.button("Click me").style("color: blue; font-size:2rem; font-style:italic")
+    #why is the numbers not working for buttons
+    ui.button("Click me",color="fuchsia-600",).props('push text-color=white').tooltip("Click me").tailwind("text-5xl hover:bg-pink-500")
+
+
+    ui.select([1, 2, 3, 4])
+
+# with ui.element('div').classes('p-8 bg-gray-100'):
+#     ui.element('button', ).classes(
+#         'bg-fuchsia-600 text-white px-4 py-2 rounded-lg hover:bg-pink-500 text-2xl'
+#     ).props('id=my-btn').inner_html('Click Me')
+
+# ui.element('h1', ).classes('text-4xl font-bold text-fuchsia-600').inner_html('This is H1')
+
+
+
+
+footer.render()
+
+
+ui.run()
---
+++ sections/footer.py
@@ -0,0 +1,5 @@
+from nicegui import ui
+
+def render():
+    with ui.row().classes('w-full justify-center p-4 bg-gray-900 text-white'):
+        ui.label('© 2025 My Landing Page. All rights reserved.')
